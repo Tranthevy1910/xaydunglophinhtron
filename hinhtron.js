dbff4fea32d92c6f3eb8a07c098117f57ad509f6
balai <balai@balainoMacBook.local>
2022-08-30 13:05:16 +0900

    lophinhtron

---
+++ hinhtron.js
@@ -0,0 +1,18 @@
+class Circle{
+    radius;
+    color;
+    constructor (radius,color){ 
+        this.radius = radius;
+        this.color = color;
+    }
+    getRadius() {
+        return this.radius;
+    }
+    getArea() { 
+        return Math.PI * this.radius * this.radius;
+    }
+}
+    let circle = new Circle(2);
+    let radius = circle.getRadius(); 
+    let area = circle.getArea();
+    alert("radius: " + radius + "; area: " + area);
